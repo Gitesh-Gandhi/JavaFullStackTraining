c0542464a5c96be9b125a54b725bbc3feb59f8c0
Gitesh Gandhi <grgandhi2000@gmail.com>
2023-04-04 14:37:23 +0530

    Springdemo

---
+++ 32-day/spring-web-demos/src/test/java/com/ani/web/springwebdemos/SpringWebDemosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ani.web.springwebdemos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebDemosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
